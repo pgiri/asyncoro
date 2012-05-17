056ed9673a7724578826c92b6934059d6f7567d3
pgiri <pgiri@users.sourceforge.net>
2012-05-17 18:14:56 -0400

    Set default log level to INFO.

--- asyncoro.py
+++ asyncoro.py
@@ -37,7 +37,7 @@ import Queue
 import atexit
 
 logger = logging.getLogger('asyncoro')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(handler)
