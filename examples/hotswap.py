fc67b1c18b452fba6e88592f39c415b9609f7f35
pgiri <pgiri@users.sourceforge.net>
2014-11-02 11:52:21 -0500

    Added example to illustrate hotswap feature.
    'hotswap' can be used to change coroutine function while application
    is running.

---
+++ examples/hotswap.py
@@ -0,0 +1,60 @@
+# Demonstratiion of hot swap feature
+
+# when 'client' command is given, the function of coroutine 'client'
+# ('client_proc') is replaced with 'client_proc2' function in the file
+# 'hotswap_funcs.py'; similarly, if 'server' command is given,
+# server's function is replaced
+
+import sys, logging, random
+import asyncoro
+
+def server_proc(coro=None):
+    coro.set_daemon()
+    # indicate that this function can be swapped
+    coro.hot_swappable(True)
+    while True:
+        try:
+            n = yield coro.receive()
+            print('%s received: %s' % (coro, n))
+        except asyncoro.HotSwapException as exc:
+            func = exc.args[0]
+            print('\nnew function: %s, %s\n' % (func.__name__, func.gi_code.co_argcount))
+            # replace only if new function meets certain criteria
+            if func.__name__.startswith('server_proc'):
+                raise
+            else:
+                print('\n** ignoring hot swap function %s' % (func.__name__))
+
+def client_proc(server, i=1, coro=None):
+    coro.set_daemon()
+    coro.hot_swappable(True)
+    # client doesn't process HotswapException
+    while True:
+        print('%s: sending %s' % (coro, i))
+        server.send(i)
+        i += 1
+        yield coro.sleep(random.uniform(1, 3))
+
+def swap(func_name, file_name, coro, *args, **kwargs):
+    try:
+        exec(open(file_name).read())
+        func = locals()[func_name]
+        coro.hot_swap(func, *args, **kwargs)
+    except:
+        print('failed to load "%s" from "%s"' % (func_name, file_name))
+
+if __name__ == '__main__':
+    asyncoro.logger.setLevel(logging.DEBUG)
+    server = asyncoro.Coro(server_proc)
+    client = asyncoro.Coro(client_proc, server)
+    while True:
+        try:
+            cmd = sys.stdin.readline().strip().lower()
+            if cmd.startswith('client'):
+                swap('client_proc2', 'hotswap_funcs.py', client, server)
+            elif cmd.startswith('server'):
+                swap('server_proc2', 'hotswap_funcs.py', server, random.choice(['log', 'sqrt']))
+            elif cmd == 'quit' or cmd == 'exit':
+                break
+        except KeyboardInterrupt:
+            break
---
+++ examples/hotswap_funcs.py
@@ -0,0 +1,31 @@
+# use with 'hotswap.py'
+
+# 'server_proc2' replaces 'server' coroutine's function
+def server_proc2(fname, coro=None):
+    import math
+    func = getattr(math, fname)
+    coro.set_daemon()
+    coro.hot_swappable(True)
+    while True:
+        try:
+            n = yield coro.receive()
+            print('%s received: %s,  %s=%.2f' % (coro, n, fname, func(n)))
+        except asyncoro.HotSwapException as exc:
+            gen = exc.args[0]
+            if gen.__name__.startswith('server_proc'):
+                print('\nreplacing with function: %s, %s\n' % \
+                      (gen.__name__, gen.gi_code.co_argcount))
+                raise
+            else:
+                print('\n** ignoring hot swap function %s' % (gen.__name__))
+
+# 'client_proc2' replaces 'client' coroutine's function
+def client_proc2(server, i=42, coro=None):
+    coro.set_daemon()
+    coro.hot_swappable(True)
+    while True:
+        print('%s: sending %s' % (coro, i))
+        server.send(i)
+        i += 2
+        yield coro.sleep(random.uniform(2, 6))
+
