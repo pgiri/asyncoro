3bc52f89e727e2895ea77d10bd6fd8cd1b640ea6
pgiri <pgiri@yahoo.com>
2015-11-29 20:51:51 -0500

    Use ProcScheduler to start 5 jobs concurrently.
    ProcScheduler is used to start 5 jobs concurrently. The exit status
    from the program is sent to client.

--- examples/discomp7.py
+++ examples/discomp7.py
@@ -12,6 +12,7 @@ import logging, random
 from asyncoro.discoro import DiscoroStatus
 import asyncoro.discoro as discoro
 import asyncoro.disasyncoro as asyncoro
+import asyncoro.discoro_schedulers
 
 # rcoro_proc is sent to remote server to execute discomp7_proc.py
 # program. It uses message passing to get data from client that is
@@ -60,49 +61,41 @@ def client_proc(computation, coro=None):
     def send_input(rcoro, coro=None):
         for i in range(10):
             rcoro.send(random.uniform(1, 4))
-            yield coro.sleep(1)
+            # assume delay in input availability
+            yield coro.sleep(random.uniform(0, 2))
         # end of input is indicated with None
         rcoro.send(None)
 
     # read output (messages sent by 'reader_proc' on remote process)
-    def get_output(coro=None):
+    def get_output(i, coro=None):
         while True:
             line = yield coro.receive()
             if not line:
                 break
-            print('output: %s' % line.strip())
+            print('job %s output: %s' % (i, line.strip()))
 
-    computation.status_coro = coro
+    # use ProcScheduler to run one discomp7_proc.py at a server
+    job_scheduler = asyncoro.discoro_schedulers.ProcScheduler(computation)
     if (yield computation.schedule()):
         raise Exception('schedule failed')
 
-    # in this example only one remote process is started
-    client_reader = asyncoro.Coro(get_output)
-    rcoro = None
-    # process status messages from scheduler
-    while True:
-        msg = yield coro.receive()
-        if isinstance(msg, DiscoroStatus):
-            if not rcoro and msg.status == discoro.Scheduler.ServerInitialized:
-                rcoro = yield computation.run_at(msg.info, rcoro_proc,
-                                                 client_reader, 'discomp7_proc.py')
-                if isinstance(rcoro, asyncoro.Coro):
-                    print('rcoro %s created' % rcoro)
-                    asyncoro.Coro(send_input, rcoro)
-                else:
-                    print('starting remote process at %s failed: %s' % (msg.info, rcoro))
-                    rcoro = None
-        elif isinstance(msg, asyncoro.MonitorException):
-            # assert str(msg.args[0]) == str(rcoro)
-            if msg.args[1][0] == StopIteration:
-                print('rcoro %s done: %s' % (msg.args[0], msg.args[1][1]))
-            else:
-                print('rcoro %s failed: %s / %s' % (msg.args[0], msg.args[1][0], msg.args[1][1]))
-            break
+    def run_instance(i, coro=None):
+        client_reader = asyncoro.Coro(get_output, i)
+        rcoro = yield job_scheduler.schedule(rcoro_proc, client_reader, 'discomp7_proc.py')
+        if isinstance(rcoro, asyncoro.Coro):
+            print('  job %s processed by %s' % (i, rcoro))
+            asyncoro.Coro(send_input, rcoro)
+            # wait for all data to be received
+            yield client_reader.finish()
+            print('  job %s done' % i)
+        else:
+            client_reader.terminate()
 
-    # wait for all data to be received
-    yield client_reader.finish()
-    yield computation.close()
+    # in this example 5 instances of discomp7_proc.py are run
+    for job in [asyncoro.Coro(run_instance, i) for i in range(1, 6)]:
+        yield job.finish()
+
+    yield job_scheduler.finish(close=True)
 
 
 if __name__ == '__main__':
